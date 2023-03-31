40e0a7c8490c2c738dbbaca2ffb8e0870e892ca2
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2023-03-31 10:45:37 +0700

    mengatur form, gambar, dan navigasi di register page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:exercise_paml/register_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,8 +9,126 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? name;
+  String? password;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          key: _formKey,
+          child: Column(children: [
+            Image.network(
+              'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png',
+              width: 250,
+              height: 250,
+            ),
+            Container(padding: const EdgeInsets.all(0)),
+            const Text(
+              "Let's get started.",
+              style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            ),
+            Container(padding: const EdgeInsets.all(5)),
+            const Text(
+              "There's No Sense In Going Out Of \n Your Way To Get Somebody To Like You.",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+            ),
+            Container(padding: const EdgeInsets.all(10)),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Enter your name',
+                labelText: 'Name',
+                prefixIcon: Icon(Icons.person),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your name';
+                } else if (value == name) {
+                  return 'Name must be at least 6 characters';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                name = value;
+              },
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                prefixIcon: Icon(Icons.lock),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                } else if (value.length < 6) {
+                  return 'Password must be at least 6 characters';
+                }
+                return null;
+              },
+              onSaved: (value) {
+                password = value;
+              },
+            ),
+            Container(padding: const EdgeInsets.all(30)),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RegisterPage(
+                          //name: name,
+                          //password: password,
+                          ),
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                minimumSize: Size(300, 50),
+              ),
+              child: const Text(
+                'Sign In',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Don't have an account?"),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterPage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
+      )),
+    );
   }
 }
